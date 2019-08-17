cae1c8523226d5a8a9b23c0d019a7cf62575ec0c
wang-www_3 <15245380670@163.com>
2019-08-17 15:35:32 +0800

    itrip

---
+++ itripauth/src/main/java/com/bdqn/biz/SentSSM.java
@@ -0,0 +1,43 @@
+package com.bdqn.biz;
+
+import com.cloopen.rest.sdk.CCPRestSmsSDK;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+
+@Service
+public class SentSSM {
+
+    public boolean setPhone(String phone,String code)
+    {
+        HashMap<String, Object> result = null;
+
+        CCPRestSmsSDK restAPI = new CCPRestSmsSDK();
+
+        restAPI.init("app.cloopen.com", "8883");
+
+        restAPI.setAccount("8aaf070869dc0b88016a0f2c95d81622", "0ba4558869344a33911deeec3a676550");
+
+        restAPI.setAppId("8aaf070869dc0b88016a0f2c96301629");
+
+        result = restAPI.sendTemplateSMS(phone,"1" ,new String[]{code,"1"});
+
+        System.out.println("SDKTestGetSubAccounts result=" + result);
+        if("000000".equals(result.get("statusCode"))){
+            //正常返回输出data包体信息（map）
+         /*   HashMap<String,Object> data = (HashMap<String, Object>) result.get("data");
+            Set<String> keySet = data.keySet();
+            for(String key:keySet){
+                Object object = data.get(key);
+                System.out.println(key +" = "+object);
+            }*/
+            return  true;
+        }else{
+            //异常返回输出错误码和错误信息
+            System.out.println("错误码=" + result.get("statusCode") +" 错误信息= "+result.get("statusMsg"));
+        }
+        return  false;
+
+    }
+
+}
